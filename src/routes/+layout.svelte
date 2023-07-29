f97ceb786b1c974aab4edba0253ed2533264874a
Dmitriy <kagno.dmitriy@gmail.com>
2023-07-29 18:27:01 -0400

    chore: add base layout and hamburger icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,33 @@
 <script lang="ts">
 	import '../app.css';
+
+	import Hamburger from '$lib/icons/Hamburger.svelte';
 </script>
 
-<slot />
+<div class="drawer2">
+	<input id="my-drawer" type="checkbox" class="drawer-toggle hidden" />
+	<div class="drawer-content">
+		<!-- Navbar -->
+		<nav class="navbar">
+			<div class=" navbar-start">
+				<label for="my-drawer" class="btn btn-primary drawer-button">
+					<Hamburger />
+				</label>
+			</div>
+		</nav>
+
+		<!-- Page content here -->
+		<slot />
+
+		<!-- Footer -->
+	</div>
+
+	<div class="drawer-side">
+		<label for="my-drawer" class="drawer-overlay" />
+		<ul class="menu p-4 w-80 h-full bg-base-200 text-base-content">
+			<!-- Sidebar content here -->
+			<li>Sidebar Item 1</li>
+			<li>Sidebar Item 2</li>
+		</ul>
+	</div>
+</div>
